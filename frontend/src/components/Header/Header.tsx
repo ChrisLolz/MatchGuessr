9e76fcc184eb31e8f692a2894e4cc03d2487f747
ChrisLolz <chrisplacey@hotmail.ca>
2024-01-30 00:04:36 -0500

    login signup buttons

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,6 +4,10 @@ const Header = () => {
     return (
         <header>
             <h1>MatchGuessr</h1>
+            <div className="auth-buttons">
+                <button>Sign Up</button>
+                <button>Log In</button>
+            </div>
         </header>
     )
 }
